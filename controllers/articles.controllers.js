3f3b4205e959af6f8bbdc8c02ebdbf14a62d4305
iamtaufik <muhtopik07@gmail.com>
2023-10-18 13:19:41 +0700

    feat: get by id and delete by id

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,27 +1,20 @@
-const {
-  createArticle,
-  getAllArticles,
-  getUpdate,
-} = require("../libs/articles.libs");
+const { createArticle, getAllArticles, getUpdate, getArticleById, deleteArticle } = require('../libs/articles.libs');
 
 module.exports = {
   createArticle: async (req, res, next) => {
     try {
       const { title, body, user_id } = req.body;
 
-      if (!title || !body)
-        return res
-          .status(400)
-          .json({ status: false, message: "data tidak lengkap" });
+      if (!title || !body) return res.status(400).json({ status: false, message: 'data tidak lengkap' });
 
       const article = await createArticle(title, body, Number(user_id));
       res.status(201).json({
         status: true,
-        message: "article berhasil dibuat",
+        message: 'article berhasil dibuat',
         data: article,
       });
     } catch (err) {
-      if (err === "user tidak ditemukan") {
+      if (err === 'user tidak ditemukan') {
         res.status(404).json({
           status: false,
           message: err,
@@ -37,10 +30,25 @@ module.exports = {
       const articles = await getAllArticles();
       res.status(200).json({
         status: true,
-        message: "All Article Data Found",
+        message: 'All Article Data Found',
         data: articles,
       });
     } catch (err) {
+      next(err);
+    }
+  },
+  getDetailArticle: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const article = await getArticleById(Number(id));
+      res.status(200).json({
+        status: true,
+        message: 'Article Data Found',
+        data: article,
+      });
+    } catch (err) {
+      if (err === 'article tidak ditemukan') return res.status(404).json({ status: false, message: 'article tidak ditemukan' });
+
       next(err);
     }
   },
@@ -55,7 +63,7 @@ module.exports = {
 
         return res.status(200).json({
           status: true,
-          message: "Updated Article Successfully",
+          message: 'Updated Article Successfully',
           data: updateArticle,
         });
       } catch (err) {
@@ -69,6 +77,24 @@ module.exports = {
       return res.status(404).json({
         status: false,
         message: err,
+        data: null,
+      });
+    }
+  },
+  deleteArticleById: async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const article = await deleteArticle(id);
+      return res.status(200).json({
+        status: true,
+        message: 'Deleted Article Successfully',
+        data: article,
+      });
+    } catch (err) {
+      // console.log(err);
+      return res.status(404).json({
+        status: false,
+        message: 'Article not found',
         data: null,
       });
     }
--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { createArticle, getAllArticles, getUpdate } = require('../controllers/articles.controllers');
+const { createArticle, getAllArticles, getUpdate, getDetailArticle, deleteArticleById } = require('../controllers/articles.controllers');
 
 router.post('/', createArticle);
 router.get('/', getAllArticles);
-router.put('/:id', getUpdate); 
+router.get('/:id', getDetailArticle);
+router.put('/:id', getUpdate);
+router.delete('/:id', deleteArticleById);
 
 module.exports = router;
